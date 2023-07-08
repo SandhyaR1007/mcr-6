43c9de190c9a666946df4bbdd2d0c6692d4e186f
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-07-08 20:09:00 +0530

    feat: implemented filter by cuisine functionality

--- src/context/FoodContext.jsx
+++ src/context/FoodContext.jsx
@@ -1,16 +1,29 @@
 import { createContext, useContext, useReducer } from "react";
-import { foodReducer, initialState } from "./foodReducer";
+import { actionTypes, foodReducer, initialState } from "./foodReducer";
 
 const FoodContext = createContext();
 
 export const FoodContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(foodReducer, initialState);
+  const selectCuisine = (selectedValue) => {
+    dispatch({
+      type: actionTypes.SELECT_CUISINE,
+      payload: selectedValue,
+    });
+  };
+  const filteredRestaurants = state.selectedCuisine
+    ? state.restaurantsList.filter(
+        ({ cuisine_id }) => cuisine_id === state.selectedCuisine
+      )
+    : state.restaurantsList;
+
   return (
     <FoodContext.Provider
       value={{
         cuisineList: state.cuisineList,
-        restaurantsList: state.restaurantsList,
+        filteredRestaurants,
         selectedCuisine: state.selectedCuisine,
+        selectCuisine,
       }}
     >
       {children}
--- src/context/foodReducer.js
+++ src/context/foodReducer.js
@@ -6,8 +6,14 @@ export const initialState = {
   selectedCuisine: "",
 };
 
+export const actionTypes = {
+  SELECT_CUISINE: "SELECT_CUISINE",
+};
+
 export const foodReducer = (state, action) => {
-  switch (action.payload) {
+  switch (action.type) {
+    case actionTypes.SELECT_CUISINE:
+      return { ...state, selectedCuisine: action.payload };
     default:
       return state;
   }
