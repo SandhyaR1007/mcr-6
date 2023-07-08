25ed79eea2a0324128da85fa1c6279196adf16d0
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-07-08 19:09:34 +0530

    add: Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Home } from "./pages";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Home />;
 };
 
 export default App;
